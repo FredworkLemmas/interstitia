ddf1d03166863caab15b1c30f5e1a05cf99d7e7e
Fred McDavid <fred@frameworklabs.us>
2026-02-25 11:33:08 -0500

    adds bundling to get around the lack of an available include mechanism

---
+++ contents/code/init.js
@@ -0,0 +1,19 @@
+/*
+ Entry point for Interstitia after modularization
+*/
+
+// compatibility and startup
+const isPlasma6 = (typeof workspace.windowList === 'function');
+console.log("interstitia: main.js execution started, isPlasma6:", isPlasma6);
+console.log("interstitia: main.js execution started at " + new Date().toISOString());
+
+// debug flags must be defined before logging/config modules
+var debugMode = true;
+var fullDebugMode = false;
+
+// global control flags/state
+var block = false;
+var mouseDragOrResizeInProgress = false;
+var mouseDragOrResizeStartingGeometry = null;
+var mouseDragOrResizeStartTime = null;
+var mouseDragOrResizeNumUpdates = null;
--- tasks.py
+++ tasks.py
@@ -1,17 +1,33 @@
 from invocate import task
 
 
+@task(name='bundle')
+def bundle(c):
+    """Bundle the modular JS files into main.js"""
+    files = [
+        'contents/code/init.js',
+        'contents/code/logging.js',
+        'contents/code/config.js',
+        'contents/code/windowing.js',
+        'contents/code/gaps.js',
+        'contents/code/cascade.js',
+        'contents/code/reaction.js',
+        'contents/code/main_loop.js'
+    ]
+    c.run(f'cat {" ".join(files)} > contents/code/main.js')
+
+
 @task(namespace='dev', name='show-logs')
 def show_logs(c, pty=True):
     #c.run('journalctl -f | grep kwin')
     c.run('journalctl -f | grep interstitia')
 
 
-@task(namespace='dev', name='install')
+@task(namespace='dev', name='install', pre=[bundle])
 def install(c):
     c.run('sh ./install.sh')
 
 
-@task(namespace='dev', name='release')
-def install(c):
+@task(namespace='dev', name='release', pre=[bundle])
+def release(c):
     c.run('sh ./package.sh')
